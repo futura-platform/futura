b176325caf64056d70894e11fe6aca69ddfbd82b
nil <tomeaton76@gmail.com>
2026-02-04 18:10:02 -0800

    Added a more generic method for attaching things to a FlowBuilder context, so that consumers can build more explicit helpers that wrap FlowBuilder

--- flowBuilder.go
+++ flowBuilder.go
@@ -18,10 +18,19 @@ type unexportedContext struct {
 
 var _ context.Context = FlowBuilder{}
 
+// WithKey attaches a "key" to the returned FlowBuilder.
+// This is useful for looping flows, in order to identify the current iteration.
+// (similar to "key" in React)
 func (b FlowBuilder) WithKey(key any) FlowBuilder {
+	return b.WithContextWrapper(func(ctx context.Context) context.Context {
+		return moment.WithIdentityKey(ctx, key)
+	})
+}
+
+func (b FlowBuilder) WithContextWrapper(wrapper func(context.Context) context.Context) FlowBuilder {
 	return FlowBuilder{
 		unexportedContext: unexportedContext{
-			Context: moment.WithIdentityKey(b, key),
+			Context: wrapper(b),
 		},
 	}
 }
