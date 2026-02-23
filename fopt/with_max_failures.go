65d63f75e070590e3fe5223dd107abf4a9b04dfc
nil <tomeaton76@gmail.com>
2026-02-23 00:45:00 -0800

    added a durable max failures helper option

---
+++ fopt/with_max_failures.go
@@ -0,0 +1,34 @@
+package fopt
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime"
+	"sync/atomic"
+
+	"github.com/futura-platform/futura"
+	"github.com/futura-platform/futura/ftype"
+)
+
+var ErrMaxFailuresReached = errors.New("max failures exceeded")
+
+var failureCountHandle = futura.NewPlainDurableHandle("failureCount", func() *atomic.Int32 { return new(atomic.Int32) })
+
+func WithMaxFailures(maxFailures int32) ftype.FlowLoopOption {
+	return func(ctx context.Context) context.Context {
+		countedCtx := failureCountHandle.ProvideContext(ctx)
+		return WithStepWrapper(func(ctx context.Context, args any, callstack []runtime.Frame, call func() (output any, err error)) (errOverride error) {
+			_, err := call()
+			if err != nil {
+				ref, persist := failureCountHandle.Use(countedCtx)
+				defer persist()
+				newFailureCount := ref.Add(1)
+				if newFailureCount > maxFailures {
+					return fmt.Errorf("%w: %w: %d", ftype.ErrCancelFlow, ErrMaxFailuresReached, newFailureCount)
+				}
+			}
+			return nil
+		})(countedCtx)
+	}
+}
---
+++ fopt/with_max_failures_test.go
@@ -0,0 +1,66 @@
+package fopt_test
+
+import (
+	"context"
+	"errors"
+	"sync/atomic"
+	"testing"
+
+	"github.com/futura-platform/futura"
+	"github.com/futura-platform/futura/fopt"
+	"github.com/futura-platform/futura/ftype/executiontype"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithMaxFailures(t *testing.T) {
+	failsNTimesStep := func(n int32) futura.ComparableMomentFn[*struct{}, *struct{}] {
+		var count atomic.Int32
+		return func(ctx context.Context, args *struct{}) (*struct{}, error) {
+			if count.Add(1) <= n {
+				return nil, errors.New("test error")
+			}
+			return nil, nil
+		}
+	}
+
+	t.Run("max failures undershot", func(t *testing.T) {
+		failsTwice := failsNTimesStep(2)
+		_, err := futura.NewFlow[any, any]().Execute(t.Context(), func(b futura.FlowBuilder, args any) (any, error) {
+			return futura.Step(b, failsTwice, nil)
+		}, nil, fopt.WithMaxFailures(2))
+		require.NoError(t, err)
+	})
+
+	t.Run("max failures reached", func(t *testing.T) {
+		failsTwice := failsNTimesStep(2)
+		_, err := futura.NewFlow[any, any]().Execute(t.Context(), func(b futura.FlowBuilder, args any) (any, error) {
+			return futura.Step(b, failsTwice, nil)
+		}, nil, fopt.WithMaxFailures(1))
+		require.ErrorIs(t, err, fopt.ErrMaxFailuresReached)
+	})
+
+	t.Run("failure count is tracked durably", func(t *testing.T) {
+		container := executiontype.NewInMemoryContainer()
+
+		// step fails twice.
+		// we set it up like this so that our flow is pure. Future detects this and will panic if it isnt.
+		step := failsNTimesStep(2)
+		exec2FailStepTest := func(ctx context.Context, f *futura.Flow[any, any], afterStep func()) (any, error) {
+			return f.Execute(ctx, func(b futura.FlowBuilder, args any) (any, error) {
+				defer afterStep()
+				return futura.Step(b, step, nil)
+			}, nil, fopt.WithMaxFailures(1))
+		}
+
+		// first execution (fails once, which should increment the failure count to 1, not triggering the max failures error yet)
+		f := futura.NewFlowFromContainer[any, any](container)
+		firstExecCtx, firstExecCancel := context.WithCancel(t.Context())
+		_, err := exec2FailStepTest(firstExecCtx, f, firstExecCancel)
+		require.ErrorIs(t, err, firstExecCtx.Err())
+
+		// second execution (fails again once, which should trigger the max failures error)
+		f = futura.NewFlowFromContainer[any, any](container)
+		_, err = exec2FailStepTest(t.Context(), f, func() {})
+		require.ErrorIs(t, err, fopt.ErrMaxFailuresReached)
+	})
+}
