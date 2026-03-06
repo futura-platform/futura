bf6a16d90832675b0cdfaf7c29bdee1bdefcfcba
nil <tomeaton76@gmail.com>
2026-03-06 15:45:38 -0800

    Updated WithOnStepError to pass a context into the callback

--- fopt/with_on_step_error.go
+++ fopt/with_on_step_error.go
@@ -8,10 +8,10 @@ import (
 	"github.com/futura-platform/futura/ftype"
 )
 
-func WithOnStepError(onError func(err error) (continueExecution bool)) ftype.FlowLoopOption {
+func WithOnStepError(onError func(ctx context.Context, err error) (continueExecution bool)) ftype.FlowLoopOption {
 	return WithStepWrapper(func(ctx context.Context, args any, callstack []runtime.Frame, call func() (output any, err error)) (errOverride error) {
 		_, err := call()
-		if err != nil && !onError(err) {
+		if err != nil && !onError(ctx, err) {
 			return fmt.Errorf("%w: %w", ftype.ErrCancelFlow, err)
 		}
 		return nil
