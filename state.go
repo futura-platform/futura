ef7696485fef47c18fbb6b9eddb1d430137bba0e
nil <tomeaton76@gmail.com>
2026-04-17 15:02:15 -0700

    Fixed edge case where the context being cancelled before and initial value was seeded caused a flow panic

--- state.go
+++ state.go
@@ -87,6 +87,16 @@ func stateWithInitialValue[T comparable](b FlowBuilder, initialValue T) StateCon
 		return nil
 	}, initialValue)
 	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			return stateContainerImplementation[T]{
+				getValue: func() T {
+					return reflect.Zero(reflect.TypeFor[T]()).Interface().(T)
+				},
+				setState: func(value T) {
+					// do nothing
+				},
+			}
+		}
 		// this should never happen
 		panic(err)
 	}
