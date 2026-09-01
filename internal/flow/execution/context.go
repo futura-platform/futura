692f8899e27539b68177e40f404cf9debdc177cd
nil <tomeaton76@gmail.com>
2026-09-01 19:22:36 -0400

    Update context.go

--- internal/flow/execution/context.go
+++ internal/flow/execution/context.go
@@ -154,10 +154,10 @@ func (f *FlowExecution) StartNewReplay(ctx context.Context) (context.Context, ui
 	return sequence.With(replay.With(ctx), flags), dirtyEpoch
 }
 
-func (f *FlowExecution) SettleSequence(ctx context.Context, atIndex int, dirtyEpoch uint64) {
+func (f *FlowExecution) SettleSequence(ctx context.Context, atIndex int, toEpoch uint64) {
 	f.mustTransact(ctx, func(ctx context.Context, tx executiontype.Container) {
 		tx.TruncateCallOrderAt(atIndex)
-		f.setEpoch(tx, evaluatedEpochKey, dirtyEpoch)
+		f.setEpoch(tx, evaluatedEpochKey, toEpoch)
 	})
 }
 
