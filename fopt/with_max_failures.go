ebb9e88d91a1571c2d354199570596cf33ed8062
nil <tomeaton76@gmail.com>
2026-09-04 23:46:33 -0400

    Fixed incorrect contact usage.

--- fopt/with_max_failures.go
+++ fopt/with_max_failures.go
@@ -21,7 +21,7 @@ func WithMaxFailures(maxFailures int32) ftype.FlowLoopOption {
 		return WithStepWrapper(func(ctx context.Context, fnLabel string, args any, callstack []runtime.Frame, call func() (output any, err error)) (errOverride error) {
 			_, err := call()
 			if err != nil {
-				newFailureCount := failureCountHandle.Use(countedCtx).Add(1)
+				newFailureCount := failureCountHandle.Use(ctx).Add(1)
 				if newFailureCount > maxFailures {
 					return fmt.Errorf("%w: %w: %d, last error: %w", ftype.ErrCancelFlow, ErrMaxFailuresReached, newFailureCount, err)
 				}
